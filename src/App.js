9c4722c2d29b1d2cf4699fdb3f562a76953e037c
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2019-08-18 00:44:09 -0400

    Card component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const user={
+  firstName:'Oriana',
+  lastName:'Gonzalez'
+}
+function getFullName(user) {
+  return `${user.firstName} ${user.lastName}`;
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hola {getFullName(user)}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './styles/Card.css'
+export default class Card extends Component {
+  render() {
+    const {title,description,bgImg}=this.props
+    return (
+      <div className="card Fitness-Card mx-auto" style={{
+        backgroundImage:`url(${bgImg}), linear-gradient(to right, #A74CF2, #617BFB)`
+      }}>
+        <div className="card-body">
+          <div className="row center">
+            <div className="col-6">
+              <img className="float-right w-100" src="https://images.vexels.com/media/users/3/130267/isolated/preview/1bd72ea2a81cf0d78f0e9640a71b9ae5-silueta-de-mujer-atleta-by-vexels.png"/>
+            </div>
+            <div className="col-6 Fitness-Card-Info" >
+              <h1>{title}</h1>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
